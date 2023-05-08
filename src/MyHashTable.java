6085f8a6c213b23246eafb5a02c8a216399ef53f
nek0722 <220439@astanait.edu.kz>
2023-05-08 21:02:49 +0600

    implemented HashNode class in MyHashTable class

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,2 +1,14 @@
-public class MyHashTable {
+public class MyHashTable<K,V> {
+    private class HashNode<K,V>{
+        private K key;
+        private V value;
+        private HashNode<K,V> next;
+        public HashNode(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+        public String toString(){
+            return " " + key + " " + value + " ";
+        }
+    }
 }
